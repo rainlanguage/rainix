f3ac68cf8ed82aa0cf85873922315b9fe42ed495
thedavidmeister <thedavidmeister@gmail.com>
2024-01-07 20:40:50 +0400

    allow additional inputs for tasks

--- flake.nix
+++ flake.nix
@@ -6,52 +6,51 @@
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
     foundry.url = "github:shazow/foundry.nix/main";
+    rain.url = "github:rainprotocol/rain.cli/6a912680be6d967fd6114aafab793ebe8503d27b";
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-utils, rust-overlay, foundry, ... }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, foundry, rain }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays =[ (import rust-overlay) foundry.overlay ];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-        forge-bin = "${pkgs.foundry-bin}/bin/forge";
-        slither-bin = "${pkgs.slither-analyzer}/bin/slither";
-        rust-bin-pin = pkgs.rust-bin.stable."1.75.0".default;
-        cargo-bin = "${rust-bin-pin}/bin/cargo";
 
         baseBuildInputs = [
           pkgs.rust-bin.stable."1.75.0".default
           pkgs.foundry-bin
           pkgs.slither-analyzer
+          rain.defaultPackage.${system}
         ] ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [
           pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
         ]);
 
         # https://ertt.ca/nix/shell-scripts/
-        mkCITask = name: pkgs.symlinkJoin {
-            name = name;
-            paths = [
-              ((pkgs.writeScriptBin name (builtins.readFile ./ci/${name}.sh)).overrideAttrs(old: {
-                buildCommand = "${old.buildCommand}\n patchShebangs $out";
-              }))
-            ] ++ baseBuildInputs;
-            buildInputs = [ pkgs.makeWrapper ];
-            postBuild = "wrapProgram $out/bin/${name} --prefix PATH : $out/bin";
-          };
+        mkTask = { name, additionalBuildInputs ? [] }: pkgs.symlinkJoin {
+          name = name;
+          paths = [
+            ((pkgs.writeScriptBin name (builtins.readFile ./task/${name}.sh)).overrideAttrs(old: {
+              buildCommand = "${old.buildCommand}\n patchShebangs $out";
+            }))
+          ] ++ baseBuildInputs ++ additionalBuildInputs;
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = "wrapProgram $out/bin/${name} --prefix PATH : $out/bin";
+        };
 
       in {
         pkgs = pkgs;
         buildInputs = baseBuildInputs;
+        mkTask = mkTask;
 
         packages = {
-          ci-sol-test = mkCITask "ci-sol-test";
-          ci-sol-artifacts = mkCITask "ci-sol-artifacts";
-          ci-sol-static = mkCITask "ci-sol-static";
+          ci-sol-test = mkTask { name = "ci-sol-test"; };
+          ci-sol-artifacts = mkTask { name = "ci-sol-artifacts"; };
+          ci-sol-static = mkTask { name = "ci-sol-static"; };
 
-          ci-rs-test = mkCITask "ci-rs-test";
-          ci-rs-artifacts = mkCITask "ci-rs-artifacts";
-          ci-rs-static = mkCITask "ci-rs-static";
+          ci-rs-test = mkTask { name = "ci-rs-test"; };
+          ci-rs-artifacts = mkTask { name = "ci-rs-artifacts"; };
+          ci-rs-static = mkTask { name = "ci-rs-static"; };
         };
 
         devShells.default = pkgs.mkShell {
