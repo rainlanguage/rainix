3bbc03daed3fca4e6fa837f4f0c948441d1aeff5
thedavidmeister <thedavidmeister@gmail.com>
2024-01-09 13:53:26 +0400

    debug deploy

--- task/rainix-sol-artifacts.sh
+++ task/rainix-sol-artifacts.sh
@@ -7,8 +7,9 @@ forge selectors up --all
 # Deploy all contracts to testnet.
 # Assumes the existence of a `Deploy.sol` script in the `script` directory.
 # Echos the deploy pubkey to stdout to make it easy to add gas to the account.
-echo 'deploy pubkey:'
+echo 'deploy pubkey:';
 cast wallet address "${DEPLOYMENT_KEY}";
+echo "ETH_RPC_URL: $ETH_RPC_URL";
 forge script script/Deploy.sol:Deploy \
     --legacy \
     --verify \
