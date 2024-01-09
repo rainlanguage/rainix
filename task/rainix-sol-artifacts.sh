7fc47af58e0a9fec9e2b7fa5c5233525950fe54f
thedavidmeister <thedavidmeister@gmail.com>
2024-01-09 14:13:34 +0400

    docs

--- task/rainix-sol-artifacts.sh
+++ task/rainix-sol-artifacts.sh
@@ -9,6 +9,8 @@ forge selectors up --all
 # Echos the deploy pubkey to stdout to make it easy to add gas to the account.
 echo 'deploy pubkey:';
 cast wallet address "${DEPLOYMENT_KEY}";
+# Need to set --rpc-url explicitly due to an upstream bug.
+# https://github.com/foundry-rs/foundry/issues/6731
 forge script script/Deploy.sol:Deploy \
     --legacy \
     --verify \
