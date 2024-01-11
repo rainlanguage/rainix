d8b9a08e2d01c4f8bc1dca8828f5685be11f5b3d
thedavidmeister <thedavidmeister@gmail.com>
2024-01-11 15:20:27 +0400

    conditional --verify

--- task/rainix-sol-artifacts.sh
+++ task/rainix-sol-artifacts.sh
@@ -11,9 +11,11 @@ echo 'deploy pubkey:';
 cast wallet address "${DEPLOYMENT_KEY}";
 # Need to set --rpc-url explicitly due to an upstream bug.
 # https://github.com/foundry-rs/foundry/issues/6731
+# Mind the bash-fu on --verify.
+# https://stackoverflow.com/questions/42985611/how-to-conditionally-add-flags-to-shell-scripts
 forge script script/Deploy.sol:Deploy \
     --legacy \
-    --verify \
+    ${ETHERSCAN_API_KEY:+--verify} \
     --broadcast \
     --rpc-url "${ETH_RPC_URL}" \
 ;
