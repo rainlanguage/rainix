b8a53841cc6bec3bbed0acaf37a35b813ba35260
thedavidmeister <thedavidmeister@gmail.com>
2024-01-11 11:02:51 +0400

    WEBKIT_DISABLE_COMPOSITING_MODE=1 for tauri

--- flake.nix
+++ flake.nix
@@ -117,6 +117,7 @@
           buildInputs = baseBuildInputs ++ tauriBuildInputs;
           shellHook =
             ''
+              export WEBKIT_DISABLE_COMPOSITING_MODE=1
               export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath tauriLibraries}:$LD_LIBRARY_PATH
               export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
             '';
