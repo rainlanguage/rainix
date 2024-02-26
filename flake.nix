f6cfc0f1116fabdfa5016f91ff6d97a9ad16cc40
thedavidmeister <thedavidmeister@gmail.com>
2024-02-27 00:37:27 +0400

    debug

--- flake.nix
+++ flake.nix
@@ -257,11 +257,11 @@
         in pkgs.mkShell {
           packages = sol-build-inputs ++ rust-build-inputs ++ node-build-inputs ++ tauri-build-inputs;
           nativeBuildInputs = [pkgs.pkg-config];
-          buildInputs = [ pkgs.gtk3 ];
+          buildInputs = [ pkgs.gtk3 pkgs.glib ];
           shellHook =
             ''
               export PATH="/usr/bin:$PATH"
-              export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath tauri-libraries}:$LD_LIBRARY_PATH
+              # export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath tauri-libraries}:$LD_LIBRARY_PATH
               export WEBKIT_DISABLE_COMPOSITING_MODE=1
               export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
             '';
