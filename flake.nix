ce9ad7e85afe59c956724f6a410914e27a19197f
Matt Gabrenya <matt@buildyourweb.app>
2024-03-07 15:06:37 -0700

    build: set DYLD_FALLBACK_LIBRARY_PATH for mac os build library deps

--- flake.nix
+++ flake.nix
@@ -273,6 +273,7 @@
               echo $PKG_CONFIG_PATH
               export PATH="/usr/bin:$PATH"
               export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath tauri-libraries}:$LD_LIBRARY_PATH
+              export DYLD_FALLBACK_LIBRARY_PATH=${pkgs.lib.makeLibraryPath tauri-libraries}:$LD_LIBRARY_PATH
               export WEBKIT_DISABLE_COMPOSITING_MODE=1
               export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
             '';
