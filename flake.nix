98c57a62930adb743d76fe332adedadf13d6d3a0
Matt Gabrenya <matt@buildyourweb.app>
2024-03-07 15:46:47 -0700

    wip: default shell set dyld

--- flake.nix
+++ flake.nix
@@ -241,6 +241,17 @@
 
         devShells.default = pkgs.mkShell {
           packages = sol-build-inputs ++ rust-build-inputs ++ rainix-tasks;
+        
+          shellHook =
+            ''
+              export DYLD_FALLBACK_LIBRARY_PATH=${pkgs.lib.makeLibraryPath tauri-libraries}:$DYLD_FALLBACK_LIBRARY_PATH
+        
+              echo "library fallback path"
+              echo $DYLD_FALLBACK_LIBRARY_PATH
+
+              echo "librar path"
+              echo $DYLD_LIBRARY_PATH
+            '';
         };
 
         # https://tauri.app/v1/guides/getting-started/prerequisites/#setting-up-linux
@@ -274,16 +285,8 @@
 
               export PATH="/usr/bin:$PATH"
               export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath tauri-libraries}:$LD_LIBRARY_PATH
-              export DYLD_FALLBACK_LIBRARY_PATH=${pkgs.lib.makeLibraryPath tauri-libraries}:$DYLD_FALLBACK_LIBRARY_PATH
               export WEBKIT_DISABLE_COMPOSITING_MODE=1
               export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
-
-              echo "library fallback path"
-              echo $DYLD_FALLBACK_LIBRARY_PATH
-
-
-              echo "librar path"
-              echo $DYLD_LIBRARY_PATH
             '';
         };
       }
