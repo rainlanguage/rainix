c0afc776fe4ecc3cc67ce0261e1af04216b3b673
Matt Gabrenya <matt@buildyourweb.app>
2024-03-11 14:20:35 -0700

    openssl pkg

--- flake.nix
+++ flake.nix
@@ -65,7 +65,7 @@
           pkgs.librsvg
           pkgs.gettext
           pkgs.libiconv
-          pkgs.openssl_3_1
+          pkgs.openssl_3_2
         ]
         ++ (pkgs.lib.optionals (!pkgs.stdenv.isDarwin) [
           # This is probably needed but is is marked as broken in nixpkgs
@@ -249,7 +249,7 @@
             pkgs.gdk-pixbuf
             pkgs.glib
             pkgs.dbus
-            pkgs.openssl_3_1
+            pkgs.openssl_3_2
             pkgs.librsvg
             pkgs.gettext
             pkgs.libiconv
@@ -267,7 +267,7 @@
           buildInputs = [pkgs.pkg-config];
           shellHook =
             ''
-              export PKG_CONFIG_PATH=${pkgs.openssl_3_1}/lib/pkgconfig:$PKG_CONFIG_PATH;
+              export PKG_CONFIG_PATH=${pkgs.openssl_3_2}/lib/pkgconfig:$PKG_CONFIG_PATH;
               echo "pkg config path"
               echo $PKG_CONFIG_PATH
               export PATH="/usr/bin:$PATH"
