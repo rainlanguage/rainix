54eb71579f9895bff7636ec863b0e954b02b2402
thedavidmeister <thedavidmeister@gmail.com>
2024-03-12 19:13:08 +0400

    debug path

--- flake.nix
+++ flake.nix
@@ -262,7 +262,6 @@
           buildInputs = sol-build-inputs ++ rust-build-inputs ++ node-build-inputs ++ tauri-build-inputs;
           shellHook =
             ''
-              export PATH="/usr/bin:$PATH"
               export WEBKIT_DISABLE_COMPOSITING_MODE=1
               export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
             '';
